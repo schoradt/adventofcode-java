3d5fb4beaaecc6444485a92711be7d32f8a62f5c
Sven Schoradt <s.schoradt@infotec-edv.de>
2019-12-10 15:34:29 +0100

    SYS: not finished stated at work

---
+++ src/main/java/de/schoradt/adventofcode2019/Day10.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2019 Sven Schoradt (s.schoradt@infotec-edv.de).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.schoradt.adventofcode2019;
+
+import de.schoradt.adventofcode2019.data.Raster;
+
+import java.util.List;
+
+/**
+ *
+ * @author Sven Schoradt (s.schoradt@infotec-edv.de)
+ */
+public class Day10 extends Day00 {
+    public class Location {
+        private final boolean asteroid;
+        
+        private int views;
+
+        public Location(boolean asteroid) {
+            this.asteroid = asteroid;
+        }
+
+        public boolean isAsteroid() {
+            return asteroid;
+        }
+
+        public int getViews() {
+            return views;
+        }
+        
+        
+    }
+    
+    
+    public int bestAstroidViewCount(List<String> lines) {
+        int count = Integer.MIN_VALUE;
+        
+        Raster<Location> raster = createRaster(lines);
+        
+        raster.print();
+        
+        for (int i = 0; i < raster.getWidth(); i++) {
+            for (int j = 0; j < raster.getHeight(); j++) {
+                if (raster.get(i, j) != null) {
+                    int views = computeViews(raster, i, j);
+                    
+                    System.out.println("views (" + i + ", " + j + ") " + views);
+                    
+                    count = Math.max(views, count);
+                }
+            }
+        }
+        
+        return count;
+    }
+    
+    private int computeViews(Raster<Location> raster, int x, int y) {
+        int views = 0;
+        
+        for (int i = 0; i < raster.getWidth(); i++) {
+            for (int j = 0; j < raster.getHeight(); j++) {
+                if (raster.get(i, j) != null && !(i == x && j == y)) {
+                    if (raster.isViewable(x, y, i, j)) {
+                        views++;
+                    };
+                }
+            }
+        }
+        
+        return views;
+    }
+    
+    private Raster<Location> createRaster(List<String> lines) {
+        Raster<Location> raster = new Raster<>(lines.get(0).length(), lines.size());
+        
+        for (int i = 0; i < lines.size(); i++) {
+            String line = lines.get(i);
+            
+            for (int j = 0; j < line.length(); j++) {
+                char c = line.charAt(j);
+                
+                if (c == '#') {
+                    raster.set(j, i, new Location(true));
+                }
+            }
+        }
+        
+        return raster;
+    }
+    
+}
---
+++ src/main/java/de/schoradt/adventofcode2019/data/Raster.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2019 Sven Schoradt (s.schoradt@infotec-edv.de).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.schoradt.adventofcode2019.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Sven Schoradt (s.schoradt@infotec-edv.de)
+ * @param <T>
+ */
+public class Raster <T> {
+    int width;
+    int height;
+    
+    private final List<List<T>> data;
+    
+    public Raster(int x, int y) {
+        width = x;
+        height = y;
+        
+        data = new ArrayList<>(x);
+        
+        for (int i = 0; i < x; i++) {
+            data.add(new ArrayList<>(y));
+            
+            for (int j = 0; j < y; j++) {
+                data.get(i).add(null);
+            }
+        }
+    }
+    
+    public T get(int x, int y) {
+        return data.get(x).get(y);
+    }
+    
+    public void set (int x, int y, T value) {
+        data.get(x).set(y, value);
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+    
+    public void print() {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (this.get(j, i) != null) {
+                    System.out.println("(" + j + ", " + i + ") #");
+                }
+            }
+        }
+        
+        System.out.println();
+        
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (this.get(j, i) != null) {
+                    System.out.print('#');
+                } else {
+                    System.out.print('.');
+                }
+            }
+            
+            System.out.println();
+        }
+        
+        System.out.println();
+    }
+    
+    public boolean isViewable(int x1, int y1, int x2, int y2) {
+        System.out.println("test (" + x1 + ", " + y1 + ") (" + x2 + ", " + y2 + ")"  );
+        
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                if (!((x1 == i && y1 == j) || (x2 == i && y2 == j))) {
+                    if (this.get(i, j) != null) {
+                        float fx1 = x1;
+                        float fx2 = x2;
+                        float fy1 = y1;
+                        float fy2 = y2;
+
+                        Float f;
+                        
+                        if (fx1 == fx2) {
+                            if (fx1 == i && (distance(x1, y1, x2, y2) > distance(x1, y1, i, j))) {
+                                System.out.println("    blocked by (" + i + ", " + j + ") ");
+
+                                return false;
+                            } else {
+                                //System.out.println("    not blocked by (" + i + ", " + j + ") ");
+                            }
+                        } else {
+                            f = ((fy2 - fy1) / (fx2 - fx1)) * i + (fy1 - ((fy2 - fy1) / (fx2 - fx1)) * fx1);
+                            
+                            if (f == j && (distance(x1, y1, x2, y2) > distance(x1, y1, i, j))) {
+                                System.out.println("    blocked by (" + i + ", " + j + ") "  + f + " <> " + j);
+
+                                return false;
+                            } else {
+                                //System.out.println("    not blocked by (" + i + ", " + j + ") "  + f + " <> " + j);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        return true;
+    }
+    
+    private double distance(int x1, int y1, int x2, int y2) {
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    }
+}
